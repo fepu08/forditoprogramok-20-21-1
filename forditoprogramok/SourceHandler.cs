7266aadb042b4ce78e5898effcd27fe5afaf22a9
fepu08 <besenyeiferenc@protonmail.com>
2020-11-27 16:23:28 +0100

    SourceHandler atszervezese

--- forditoprogramok/SourceHandler.cs
+++ forditoprogramok/SourceHandler.cs
@@ -32,6 +32,9 @@ namespace forditoprogramok
         private Dictionary<string, string> replacesDictionary = new Dictionary<string, string>();
         private List<string> symbolTable = new List<string>();
         private int symbolIndex = 0;
+        private static string patternNumber = @"([0-9]+)";
+        private static string patternVar = @"([a-z-_]+)";
+
         public string FilePathToRead 
         {
             get { return filePathToRead; }
@@ -39,7 +42,7 @@ namespace forditoprogramok
             {
                 if (true)
                 {
-                    filePathToRead = value; 
+                    filePathToRead = value;
                 } else
                 {
                     throw new Exception("Hibás érték");
@@ -71,6 +74,7 @@ namespace forditoprogramok
                 if (true)
                 {
                     content = value;
+                    PurgeSymbolTable();
                 }
                 else
                 {
@@ -84,10 +88,10 @@ namespace forditoprogramok
             this.filePathToRead = filePathToRead;
             this.filePathToWrite = filePathToWrite;
             this.dictionaryPath = dictionaryPath;
-            this.readDictionary();
+            this.ReadDictionary();
         }
 
-        public bool openFileToRead()
+        public bool OpenFileToRead()
         {
             try
             {
@@ -103,7 +107,7 @@ namespace forditoprogramok
             return true;
         }
 
-        public bool openFileToWrite()
+        public bool OpenFileToWrite()
         {
             try
             {
@@ -120,7 +124,7 @@ namespace forditoprogramok
             return true;
         }
 
-        private void readDictionary()
+        private void ReadDictionary()
         {
             try
             {
@@ -144,25 +148,39 @@ namespace forditoprogramok
         /** replaceContent(s)
          * A megfelelő módon majd felülírjuk a content változó tartalmát
          */
-        public void replaceContent()
+
+        public void Replace()
         {
-            content = content.ToLower(); // for avoid case sensitivity
-            //var blockComments = @"/\*(.*?)\*/";
-            //var blockComments = @"^/[/|*](.+)$";
-            //var lineComments = @"//(.*?)\r?\n";
+            ClearComments();
+            content = Regex.Replace(content, "\n", String.Empty);
+            content = Regex.Replace(content, "\r", String.Empty);
+            ReplaceNumbers();
+            ReplaceVariables(); // Ha a kód nincs nagybetűvel írva, azokat is változónak érzékeli (pl if -> VAR[if])
+            ReplaceUsingDictionary();
+            StoreVariablesAndConstants(content);
+            RemoveSpaces();
+        }
+
+        public void ClearComments()
+        {
             string patternBlockComment = @"/[*][\w\d\s]+[*]/";
             string patternLineComment = @"//.*?\n";
-            string patternNumber = @"([0-9]+)";
-            string patternVar = @"([a-z-_]+)";
-            string replaceNumber = " CONST[$1] ";
-            string replaceVar = " VAR[$1] ";
-
             content = Regex.Replace(content, patternBlockComment, String.Empty);
             content = Regex.Replace(content, patternLineComment, String.Empty);
-            content = Regex.Replace(content, "\r", String.Empty);
-            content = Regex.Replace(content, patternNumber, changeVariablesAndConstants("$1")); // Ez a $1 es megoldás nem igazán működik
-            content = Regex.Replace(content, patternVar, changeVariablesAndConstants("$1"));
+        }
 
+        public void ReplaceNumbers()
+        {
+            content = Regex.Replace(content, patternNumber, " CONST[$0] ");
+        }
+
+        public void ReplaceVariables()
+        {
+            content = Regex.Replace(content, patternVar, " VAR[$0] ");
+        }
+
+        public void ReplaceUsingDictionary()
+        {
             foreach (var x in replacesDictionary)
             {
                 while (content.Contains(x.Key))
@@ -170,86 +188,56 @@ namespace forditoprogramok
                     content = content.Replace(x.Key, x.Value);
                 }
             }
+        }
 
-            // Ha az első karakter space levágjuk
-            if(content[0] == ' ') { content.Substring(0); }
+        public void StoreVariablesAndConstants(string data)
+        {
+            string[] contentArray = data.Split(' ');
+            string tempPattern = string.Format($@"(VAR\[.*\]+|CONST\[.*\]+)");
+            foreach (var symbol in contentArray)
+            {
+                if (Regex.IsMatch(symbol, tempPattern))
+                {
+                    string tempSymbol;
+                    if (symbol.Contains("VAR"))
+                    {
+                        int start = symbol.IndexOf("VAR");
+                        tempSymbol = symbol.Substring(start, symbol.IndexOf("]") - start + 1);
+                    }
+                    else
+                    {
+                        int start = symbol.IndexOf("CONST");
+                        tempSymbol = symbol.Substring(start, symbol.IndexOf("]") - start + 1);
+                    }
+
+                    symbolTable.Add(tempSymbol);
+                    symbolIndex += 1;
+                    string response = "00" + symbolIndex.ToString();
+                    content = content.Replace(tempSymbol, response.Substring(response.Length - 3));
+                }
+            }
+        }
+
+        public void RemoveSpaces()
+        {
+            while (content[0] == ' ') { content = content.Substring(1); }
             // Előfordult, hogy dupla space lett pl if(..) itt { .. ezért
             content = Regex.Replace(content, "  ", " ");
         }
 
-        /* KIINDULÁS - beolvasása openFileToRead() metódussal
-         * int       i =   10; // asdlkhawpihn
-         * while (i < 10 ) {
-         *  i++;
-         * }
-         */
-        //"int i=10;while(i<10){i++;}" ehhez hasonló VÉGEREDMÉNY
-
-        string changeVariablesAndConstants(string varAndConstName)
-        {
-            symbolTable.Add(varAndConstName);
-            symbolIndex++;
-            string result = "00" + symbolIndex.ToString();
-            return result.Substring(result.Length - 3);
-        }
-
         /** replaceText() - Feladata, hogy kicseréljen bármilyen szöveget bármire*/
-        public void replaceText(string from, string to)
+        public void ReplaceText(string from, string to)
         {
             while (content.Contains(from))
             {
                 content = content.Replace(from, to);
             }
         }
-        
-        public void replaceFirst()
+
+        private void PurgeSymbolTable()
         {
-            //content = Replace(content, @"[a-zA-Z0-9]", "VARIABLE");
-            //content = Regex.Replace(content, @"[0-9]", "CONST");
-            //TODO: betenni egy fileba és soronként feldolgozni
-            //replacesDictionary.Add("  ", " ");
-            //replacesDictionary.Add("\n", " ");
-            //replacesDictionary.Add("    ", " ");
-            //replacesDictionary.Add(" {", "{");
-            //replacesDictionary.Add(" }", "}");
-            //replacesDictionary.Add("} ", "}");
-            //replacesDictionary.Add("{ ", "{");
-            //replacesDictionary.Add(" (", "(");
-            //replacesDictionary.Add("( ", "(");
-            //replacesDictionary.Add(" )", ")");
-            //replacesDictionary.Add(") ", ")");
-            //replacesDictionary.Add(" ;", ";");
-            //replacesDictionary.Add("; ", ";");
-            //replacesDictionary.Add(" =", "=");
-            //replacesDictionary.Add("= ", "=");
-            //replacesDictionary.Add("if", " 10 ");
-            //replacesDictionary.Add("else", " 11 ");
-            //replacesDictionary.Add("for", " 20 ");
-            //replacesDictionary.Add("while", " 21 ");
-            //replacesDictionary.Add("switch", " 30 ");
-            //replacesDictionary.Add("case", " 31 ");
-            //replacesDictionary.Add("(", " 40 ");
-            //replacesDictionary.Add(")", " 41 ");
-            //replacesDictionary.Add("==", " 50 ");
-            //replacesDictionary.Add(">", " 51 ");
-            //replacesDictionary.Add(">=", " 52 ");
-            //replacesDictionary.Add("<", " 53 ");
-            //replacesDictionary.Add("<=", " 54 ");
-            //replacesDictionary.Add("{", " 60 ");
-            //replacesDictionary.Add("}", " 61 ");
-            //replacesDictionary.Add("=", " 70 ");
-            //replacesDictionary.Add("+", " 71 ");
-            //replacesDictionary.Add("++", " 72 ");
-            //replacesDictionary.Add("-", " 73 ");
-            //replacesDictionary.Add("--", " 74 ");
-            //replacesDictionary.Add("-=", " 75 ");
-            //replacesDictionary.Add("+=", " 76 ");
-            //replacesDictionary.Add("true", " 80 ");
-            //replacesDictionary.Add("false", " 81 ");
-            //foreach (KeyValuePair<string, string> kvp in replacesDictionary)
-            //{
-            //    replaceText(kvp.Key, kvp.Value);
-            //}
+            symbolTable.Clear();
+            symbolIndex = 0;
         }
     }
 }
