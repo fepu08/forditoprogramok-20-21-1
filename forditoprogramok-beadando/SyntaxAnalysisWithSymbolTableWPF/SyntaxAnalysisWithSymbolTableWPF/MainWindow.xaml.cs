c029f7e1017f3a2314af30613c7a2fc6746d2023
fepu08 <ferencbesenyei@protonmail.com>
2020-12-09 18:35:55 +0100

    you can open .xlsx and .xls files, change between sheets and the DataGrid get the data from it

--- forditoprogramok-beadando/SyntaxAnalysisWithSymbolTableWPF/SyntaxAnalysisWithSymbolTableWPF/MainWindow.xaml.cs
+++ forditoprogramok-beadando/SyntaxAnalysisWithSymbolTableWPF/SyntaxAnalysisWithSymbolTableWPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExcelDataReader;
+using Microsoft.Win32;
 
 namespace SyntaxAnalysisWithSymbolTableWPF
 {
@@ -20,6 +24,7 @@ namespace SyntaxAnalysisWithSymbolTableWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataSet result;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,14 +40,57 @@ namespace SyntaxAnalysisWithSymbolTableWPF
 
         }
 
-        private void AddRowButton_Click(object sender, RoutedEventArgs e)
+        private void read_file_button_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|*.xlsx;*.xls", ValidateNames = true };
+            if(ofd.ShowDialog() == true)
+            {
+                FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+                IExcelDataReader reader = ExcelReaderFactory.CreateReader(fs);
+                result = reader.AsDataSet();
+                sheetCbox.Items.Clear();
+                foreach(DataTable dt in result.Tables)
+                {
+                    sheetCbox.Items.Add(dt.TableName);
+                }
+                reader.Close();
 
+                changeMessagesLabelContent("Excel workbook opened", AlertType.SUCCESS);
+            } else
+            {
+                changeMessagesLabelContent("Can't open Excel workbook", AlertType.DANGER);
+            }
+            
         }
 
-        private void AddColumnButton_Click(object sender, RoutedEventArgs e)
+        private void sheetCbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            symbol_table_data_grid.DataContext = result.Tables[sheetCbox.SelectedIndex].DefaultView;
+            disableDataGridColumnsSorting(symbol_table_data_grid);
+            changeMessagesLabelContent("Sheet changed", AlertType.SUCCESS);
+        }
 
+        private void disableDataGridColumnsSorting(DataGrid dataGridId)
+        {
+            foreach (DataGridColumn column in dataGridId.Columns)
+            {
+                column.CanUserSort = false;
+            }
+        }
+
+        private void changeMessagesLabelContent(string message, AlertType alertType = AlertType.DEFAULT)
+        {
+            if (alertType == AlertType.DEFAULT)
+            {
+                messages_label.Foreground = Brushes.Black;
+            } else if (alertType == AlertType.SUCCESS)
+            {
+                messages_label.Foreground = Brushes.Green;
+            } else if (alertType == AlertType.DANGER)
+            {
+                messages_label.Foreground = Brushes.Red;
+            }
+            messages_label.Content = message;
         }
     }
 }
---
+++ forditoprogramok-beadando/SyntaxAnalysisWithSymbolTableWPF/redme.md
@@ -0,0 +1,3 @@
+You can open an Excel workbook with *.xlsx or *.xls extensions
+
+Add ExcelDataReader and ExcelDataReader.DataSet NuGet package to C# project and rebuild it
